40323caa23f95dc767e48523f11738890620290b
罗琦 <luoqi04@meituan.com>
2018-08-17 14:06:39 +0800

    ForkJoinSumCalculator增加注释

--- src/main/java/lambdasinaction/chap7/ForkJoinSumCalculator.java
+++ src/main/java/lambdasinaction/chap7/ForkJoinSumCalculator.java
@@ -14,10 +14,12 @@ public class ForkJoinSumCalculator extends RecursiveTask<Long> {
     private final int start;
     private final int end;
 
+    //用于创建主任务
     public ForkJoinSumCalculator(long[] numbers) {
         this(numbers, 0, numbers.length);
     }
 
+    //用于以递归方式为主任务创建子任务
     private ForkJoinSumCalculator(long[] numbers, int start, int end) {
         this.numbers = numbers;
         this.start = start;
@@ -27,13 +29,18 @@ public class ForkJoinSumCalculator extends RecursiveTask<Long> {
     @Override
     protected Long compute() {
         int length = end - start;
+        //如果小于阈值则使用串行
         if (length <= THRESHOLD) {
             return computeSequentially();
         }
+        //创建一个子任务来为数组的前一半求和
         ForkJoinSumCalculator leftTask = new ForkJoinSumCalculator(numbers, start, start + length/2);
+        //利用另一个ForkJoinPool线程异步执行新创建的子任务
         leftTask.fork();
         ForkJoinSumCalculator rightTask = new ForkJoinSumCalculator(numbers, start + length/2, end);
+        //同步执行第二个子任务，有可能允许进一步递归划分
         Long rightResult = rightTask.compute();
+        //读取第一个子任务的结果，如果尚未完成就等待。阻塞
         Long leftResult = leftTask.join();
         return leftResult + rightResult;
     }
