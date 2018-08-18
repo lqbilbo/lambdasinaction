9fe5bdb4ea3473103dfd357836c59f58a8e55aae
罗琦 <luoqi04@meituan.com>
2018-08-18 16:43:21 +0800

    WordCount中Spliterator实现增加注释

--- src/main/java/lambdasinaction/chap7/WordCount.java
+++ src/main/java/lambdasinaction/chap7/WordCount.java
@@ -83,19 +83,25 @@ public class WordCount {
 
         @Override
         public boolean tryAdvance(Consumer<? super Character> action) {
+            //处理当前字符
             action.accept(string.charAt(currentChar++));
+            //如果还有字符要处理，则返回true
             return currentChar < string.length();
         }
 
         @Override
         public Spliterator<Character> trySplit() {
             int currentSize = string.length() - currentChar;
+            //返回null表示要解析的String已经足够小，可以顺序处理
             if (currentSize < 10) {
                 return null;
             }
+            //试探拆分位置改为中间位置
             for (int splitPos = currentSize / 2 + currentChar; splitPos < string.length(); splitPos++) {
                 if (Character.isWhitespace(string.charAt(splitPos))) {
+                    //创建一个新的spliterator来解析
                     Spliterator<Character> spliterator = new WordCounterSpliterator(string.substring(currentChar, splitPos));
+                    //将起始位置设置为拆分位置
                     currentChar = splitPos;
                     return spliterator;
                 }
