30814ddef4cc782c15879b611ac641830b9e0aa2
罗琦 <luoqi04@meituan.com>
2018-09-02 21:41:22 +0800

    匿名函数和Lambda表达式

--- src/main/java/lambdasinaction/appd/InnerClass.java
+++ src/main/java/lambdasinaction/appd/InnerClass.java
@@ -2,6 +2,10 @@ package lambdasinaction.appd;
 
 import java.util.function.Function;
 
+/**
+ * 每个匿名类都会产生一个新的.class文件
+ * 每个匿名类都会为类或者接口产生一个新的子类型
+ */
 public class InnerClass {
     Function<Object, String> f = new Function<Object, String>() {
         @Override
