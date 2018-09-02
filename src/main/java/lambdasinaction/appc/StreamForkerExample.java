1556d36788114ab44685bc152add18026247604d
罗琦 <luoqi04@meituan.com>
2018-09-02 21:14:19 +0800

    StreamForker探究+注释

--- src/main/java/lambdasinaction/appc/StreamForkerExample.java
+++ src/main/java/lambdasinaction/appc/StreamForkerExample.java
@@ -17,21 +17,20 @@ public class StreamForkerExample {
     private static void processMenu() {
         Stream<Dish> menuStream = menu.stream();
 
-        StreamForker.Results results = new StreamForker<Dish>(menuStream)
+        StreamForker.Results results = new StreamForker<>(menuStream)
                 .fork("shortMenu", s -> s.map(Dish::getName).collect(joining(", ")))
                 .fork("totalCalories", s -> s.mapToInt(Dish::getCalories).sum())
-                .fork("mostCaloricDish", s -> s.collect(
-                        reducing((d1, d2) -> d1.getCalories() > d2.getCalories() ? d1 : d2))
+                .fork("mostCaloricDish", s -> s.reduce((d1, d2) -> d1.getCalories() > d2.getCalories() ? d1 : d2)
                         .get())
                 .fork("dishesByType", s -> s.collect(groupingBy(Dish::getType)))
                 .getResults();
 
-        String shortMeny = results.get("shortMenu");
+        String shortMenu = results.get("shortMenu");
         int totalCalories = results.get("totalCalories");
         Dish mostCaloricDish = results.get("mostCaloricDish");
         Map<Dish.Type, List<Dish>> dishesByType = results.get("dishesByType");
 
-        System.out.println("Short menu: " + shortMeny);
+        System.out.println("Short menu: " + shortMenu);
         System.out.println("Total calories: " + totalCalories);
         System.out.println("Most caloric dish: " + mostCaloricDish);
         System.out.println("Dishes by type: " + dishesByType);
